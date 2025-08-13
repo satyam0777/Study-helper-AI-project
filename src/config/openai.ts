ded39698191b6dabdbd1f1bd85c5a53f6d92b212
satyam0777 <officialsatyam0777@gmail.com>
2025-08-13 21:56:33 +0530

    open api key set

--- src/config/openai.ts
+++ src/config/openai.ts
@@ -13,3 +13,19 @@ const openai = new OpenAI({
 
 export default openai;
 
+
+// import { GoogleGenerativeAI } from '@google/generative-ai';
+// import { env } from './env';
+
+// const geminiApiKey = process.env.GEMINI_API_KEY;
+// if (!geminiApiKey) {
+//   throw new Error('GEMINI_API_KEY is not defined in environment variables');
+// }
+// const genAI = new GoogleGenerativeAI(geminiApiKey);
+
+// export async function askGemini(prompt: string) {
+//   const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+//   const result = await model.generateContent(prompt);
+//   const response = result.response;
+//   return response.text();
+// }
